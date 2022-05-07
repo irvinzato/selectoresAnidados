a63d3f2fce6cbcf93bfb31853227b04d18016db9
irvinzato <multizato@hotmail.com>
2022-05-06 23:17:29 -0500

    Se mejoro el proceso de selecciones, es una manera de resolverlo, asi el usuario podra seleccionar en orden los selectores y se creo un spiner en el HTML, ademas de sus respectivas condiciones

--- src/app/paises/pages/selector-pages/selector-pages.component.ts
+++ src/app/paises/pages/selector-pages/selector-pages.component.ts
@@ -23,12 +23,15 @@ export class SelectorPagesComponent implements OnInit {
   paises: PaisSmall[] = [];
   fronteras: string[] = [];
 
+  //UI
+  cargando: boolean = false;
+
   constructor( private fb: FormBuilder,
                private paisesService: PaisesService ) { }
 
   ngOnInit() {
     this.regiones = this.paisesService.regiones;
-    console.log("regiones tiene ", this.regiones);
+    //console.log("regiones tiene ", this.regiones);
 
     //PRIMER MANERA DE OBTENER VALORES DEL SELECTOR Y SUSCRIBIRSE DESPUES AL SERVICIO PARA TENER SUS VALORES
     //OBTENGO EXACTAMENTE EL CAMPO "region" CUANDO TIENE ALGUN CAMBIO
@@ -46,12 +49,14 @@ export class SelectorPagesComponent implements OnInit {
     this.miFormulario.get('region').valueChanges 
     .pipe(
       tap( (_) => {   //La nomenclatura "(_)" quiere decir que no me importa lo que regrese, en este caso regresaria la "region" seleccionada
+        this.cargando = true;
         this.miFormulario.get('pais').reset('');      //No me interesa lo que regrese por que solo quiero resetear ese campo de mi formulario cuando region tenga algun cambio
       }),
       switchMap( region => this.paisesService.getPaisesPorRegion(region) )
     )
     .subscribe( pais => {
-      console.log(pais)
+      console.log(pais);
+      this.cargando = false;
       this.paises = pais;
     });
 
@@ -60,17 +65,25 @@ export class SelectorPagesComponent implements OnInit {
     this.miFormulario.get('pais').valueChanges
     .pipe(
       tap( () => {   //Otras manera de decir que no me importa el argumento
-        //this.fronteras = [];
+        this.fronteras = [];
+        this.cargando = true;
         this.miFormulario.get('frontera').reset('');      //Ahora reseteo el valor de "frontera" cuando cambie "pais"
       }),
       switchMap( codigo => this.paisesService.getPaisPorCodigo(codigo) )
     )
     .subscribe( res  => {
       if(res === null){   //Por si tiene null mi respuesta no de error el programa
-        return 
+        this.cargando = false;
+        return;
+      }
+      if(res[0].borders === undefined){   //Quiere decir que no tiene borders o "FRONTERAS" el pais seleccionado
+        this.fronteras = ['No tiene fronteras'];
+        this.cargando = false;
+        return;
       }
         console.log("respuesta del servicio getPaisPorCodigo ", res);
         this.fronteras = res[0].borders;
+        this.cargando = false;
         console.log("arreglo fronteras tiene ", this.fronteras);
     });
 
